f82d7936cd296f81130a1a5854b330ebb1f7dfce
Nicolas Cleiton <nicolascleiton700e@gmail.com>
2022-02-18 18:10:49 -0300

    Cracha NLW

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -5,7 +5,7 @@ export default function Avatar(props){
   return (
     <AvatarPhoto>
       <span>
-          <img src="../src/images/logo.svg" alt="Selo Rockeseat" />
+          <img src="./images/logo.svg" alt="Selo Rockeseat" />
       </span>
       <img id="userImg" src={props.image} alt="Imagem pessoal" />
     </AvatarPhoto>
